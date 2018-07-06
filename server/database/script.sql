12927bf8733decef805acff1a6d45d3085a151b1
wacsystems <wac.0013@gmail.com>
2018-07-05 23:59:09 -0300

    Normalizando arquivo script.sql
    Removendo acentos e usando padrão CamelCase para nome de colunas

--- server/database/script.sql
+++ server/database/script.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE `PW`;
-CREATE TABLE IF NOT EXISTS `projeto_pw`.`Ocorrência` (
-  `idOcorrência` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `pw`.`ocorrencias` (
+  `IdOcorrencia` INT NOT NULL AUTO_INCREMENT,
   `Categoria` ENUM('OBJ', 'VEI', 'PET', 'PES') NOT NULL,
   `Local` VARCHAR(45) NOT NULL,
-  `Descrição` VARCHAR(255) NOT NULL,
+  `DescriÃ§Ã£o` VARCHAR(255) NOT NULL,
   `Recompensa` FLOAT NOT NULL DEFAULT 0,
   `Telefone` VARCHAR(14) NULL,
   `StatusPerdido` ENUM('P', 'E') NOT NULL,
   `Imagens` BLOB NULL,
-  PRIMARY KEY (`idOcorrência`))
+  PRIMARY KEY (`IdOcorrencia`))
 ENGINE = InnoDB
